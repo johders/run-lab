d7f7aa6909f15c98e4647e1de7577eb9c6f7444a
johders <169034808+johders@users.noreply.github.com>
2026-01-03 12:43:44 +0100

    update run activity mapping

--- backend/RunLab.Core/Services/RunActivityMapper.cs
+++ backend/RunLab.Core/Services/RunActivityMapper.cs
@@ -6,6 +6,10 @@ namespace RunLab.Core.Services;
 
 public static class RunActivityMapper
 {
+    public static List<GarminRunActivity> ToGarminRunActivities(this List<GarminActivity> activities)
+    {
+        return [.. activities.Select(a => ToGarminRunActivity(a))];
+    }
     public static GarminRunActivity ToGarminRunActivity(this GarminActivity activity)
     {
         return new GarminRunActivity
@@ -57,7 +61,66 @@ public static class RunActivityMapper
             },
             Power = new PowerSummary
             {
-
+                AverageWatts = activity.AvgPower,
+                MaxWatts = activity.MaxPower,
+                NormalizedWatts = activity.NormalizedPower,
+                WindCorrectionEnabled = activity.IsRunPowerWindDataEnabled,
+                TimeInZones = new Dictionary<int, TimeSpan>
+                {
+                    { 0, TimeSpan.FromSeconds(activity.PowerZone0Seconds) },
+                    { 1, TimeSpan.FromSeconds(activity.PowerZone1Seconds) },
+                    { 2, TimeSpan.FromSeconds(activity.PowerZone2Seconds) },
+                    { 3, TimeSpan.FromSeconds(activity.PowerZone3Seconds) },
+                    { 4, TimeSpan.FromSeconds(activity.PowerZone4Seconds) },
+                    { 5, TimeSpan.FromSeconds(activity.PowerZone5Seconds) },
+                }
+                .Where(kvp => kvp.Value > TimeSpan.Zero)
+                .ToDictionary()
+            },
+            RunningDynamics = new RunningDynamicsSummary
+            {
+                AverageCadenceSpm = activity.AvgDoubleCadenceSpm,
+                MaxCadenceSpm = activity.MaxDoubleCadenceSpm,
+                AverageSingleLegCadenceSpm = activity.AvgRunCadenceSpm,
+                AverageStrideLengthMeters = activity.AvgStrideLengthCm / 100,
+                AverageGroundContactTimeMs = activity.AvgGroundContactTimeMs,
+                AverageVerticalOscillationCm = activity.AvgVerticalOscillationCm,
+                AverageVerticalRatioPercent = activity.AvgVerticalRatioPercent,
+                MaxVerticalSpeedMps = activity.MaxVerticalSpeedMps
+            },
+            TrainingEffect = new TrainingEffectSummary
+            {
+                AerobicTrainingEffect = activity.AerobicTrainingEffect,
+                AnaerobicTrainingEffect = activity.AnaerobicTrainingEffect,
+                TrainingLoad = activity.ActivityTrainingLoad,
+                Vo2MaxEstimate = activity.Vo2MaxValue,
+                TrainingEffectLabel = activity.TrainingEffectLabel,
+                AerobicEffectMessage = activity.AerobicTrainingEffectMessage,
+                AnaerobicEffectMessage = activity.AnaerobicTrainingEffectMessage
+            },
+            Energy = new EnergySummary
+            {
+                ModerateIntensityMinutes = activity.ModerateIntensityMinutes,
+                VigorousIntensityMinutes = activity.VigorousIntensityMinutes,
+                BodyBatteryDelta = activity.BodyBatteryDelta,
+                WorkoutFeel = activity.WorkoutFeel,
+                WorkoutRpe = activity.WorkoutRpe,
+                Calories = activity.Calories,
+                BmrCalories = activity.BmrCalories,
+                WaterEstimatedMl = activity.WaterEstimatedMl
+            },
+            Location = new LocationSummary
+            {
+                LocationName = activity.LocationName,
+                Start = new GeoPoint(activity.StartLatitude, activity.StartLongitude),
+                End = new GeoPoint(activity.EndLatitude, activity.EndLongitude),
+                BoundingBox = new GeoBoundingBox
+                {
+                    MinLatitude = activity.MinLatitude,
+                    MaxLatitude = activity.MaxLatitude,
+                    MinLongitude = activity.MinLongitude,
+                    MaxLongitude = activity.MaxLongitude
+                }
             }
         };
     }
